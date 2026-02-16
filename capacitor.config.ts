9dd28b98c11a60bd57ec2578d09fe1ad5bbc72cb
MHenriquezD <mdhenriquez16@gmail.com>
2026-02-16 16:36:02 -0600

    Añadido nuevo icono a capacitor para la apk en android, aun falta terminar la version en IOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.mhenriquez.streamdeck',
-  appName: 'stream-deck-app',
+  appId: 'com.mhenriquez.spartanhub',
+  appName: 'spartan-hub-app',
   webDir: 'dist',
 }
 
